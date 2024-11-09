a0f4aa42eadc01de17b56aff1813939db9f0962f
sinemgrbz <sinemcobanligurbuz@gmail.com>
2024-11-09 20:51:24 +0300

    fixed deployment issue

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import App from './App.jsx'
 import { BrowserRouter, HashRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>
+  </HashRouter>
 )
